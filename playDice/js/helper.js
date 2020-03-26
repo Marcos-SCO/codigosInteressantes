e3153f98d6cc94a6a248bdac6400e8b9251e228d
Marcos-SCO <marcos_sco@outlook.com>
2020-03-26 19:18:58 -0300

    Algoritimos

--- playDice/js/helper.js
+++
@@ -1,9 +0,0 @@
-function qSelect(item) {
-    return document.querySelector(item);
-}
-function qSelectAll(item) {
-    return document.querySelectorAll(item);
-}
-function log(item) {
-    return console.log(item);
-}
--- playDice/js/randomNumber.js
+++
@@ -1,4 +0,0 @@
-// Function 
-function randomNumber(n1 = 10, n2 = 1) {
-    return +(Math.floor(n1 * Math.random()) + n2);
-}
