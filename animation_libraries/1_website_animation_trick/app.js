4c481fefdcfdff4a8bc1e91a976b0d5ddfadde25
Marcos-SCO <marcos_sco@outlook.com>
2020-12-03 13:10:35 -0300

    animation libraries

---
+++ animation_libraries/1_website_animation_trick/app.js
@@ -0,0 +1,8 @@
+const tl = gsap.timeline({ defaults: { ease: 'power1.out' } })
+
+// (to) receive the element tha will be animated
+tl.to('.text', { y: "0%", duration: 1, stagger: .25 });
+tl.to('.slider', { y: "-100%", duration: 1.5, delay: .5 })
+tl.to('.intro', { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
