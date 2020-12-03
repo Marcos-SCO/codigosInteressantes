defb190cdec9f9aa7d01caee49b8789e5b69c086
Marcos-SCO <marcos_sco@outlook.com>
2020-12-03 12:19:09 -0300

    php router

--- 1_php-router/IRequest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-interface IRequest
-{
-    // getBody() retrieves data from the request body. The Request class must have the implementation for this method.
-    public function getBody();
-}
