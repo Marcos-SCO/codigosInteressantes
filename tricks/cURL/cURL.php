fb8b0f61f7ae31d791983a45b456a625b5bdd57e
Marcos-SCO <marcos_sco@outlook.com>
2020-12-05 13:08:49 -0300

    cURL options

--- tricks/cURL/cURL.php
+++ tricks/cURL/cURL.php
@@ -2,6 +2,13 @@
 
 // Consuming api's with curl
 
+/*
+curl_init(); initializes a cURL session
+curl_setopt(); changes the cURL session behavior with options
+curl_exec(); executes the started cURL session
+curl_close(); closes the cURL session and deletes the variable made by curl_init();
+*/
+
 $url = 'requestedUrl';
 
 // Init cURL with url
