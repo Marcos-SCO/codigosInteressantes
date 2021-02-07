7127e8b7787bfc79d417637274854e4365d53ae3
Marcos-SCO <marcos_sco@outlook.com>
2021-02-07 17:25:03 -0300

    toggle navigation

--- css_tricks/navigation_menus/nav_menu_background_change/app.js
+++ css_tricks/navigation_menus/nav_menu_background_change/app.js
@@ -1,4 +1,6 @@
+let navigation = document.querySelector('.navigation');
 let navLinks = document.querySelectorAll('.nav-area ul li a');
+let menuToggle = document.querySelector('.menu-toggle');
 
 navLinks.forEach(link => {
     link.addEventListener('click', e => {
@@ -9,4 +11,10 @@ navLinks.forEach(link => {
 function changeImg(img) {
     document.getElementById('source-set').srcset = img;
     document.getElementById('img-slider').src = img;
+}
+
+menuToggle.onclick = toggleMenu;
+function toggleMenu() {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
 }
